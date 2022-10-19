0583e1c28b9cf58a23150712970abb93d23e661f
CMiSSioN <47270663+CMiSSioN@users.noreply.github.com>
2022-10-19 14:15:14 +0300

    jumps folding

--- TravelInfoTooltips/Core.cs
+++ TravelInfoTooltips/Core.cs
@@ -15,21 +15,6 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace TravelInfoTooltips{
-  public class TravelInfoTooltip: EventTrigger {
-    public override void OnPointerEnter(PointerEventData data) {
-      Log.M?.TWL(0, "TravelInfoTooltip.OnPointerEnter",true);
-    }
-    public override void OnPointerExit(PointerEventData data) {
-      Log.M?.TWL(0, "TravelInfoTooltip.OnPointerExit", true);
-    }
-    public override void OnPointerDown(PointerEventData data) {
-      Log.M?.TWL(0, "TravelInfoTooltip.OnPointerDown", true);
-    }
-    public override void OnPointerUp(PointerEventData data) {
-      Log.M?.TWL(0, "TravelInfoTooltip.OnPointerUp", true);
-    }
-
-  }
   [HarmonyPatch(typeof(SGSystemViewPopulator))]
   [HarmonyPatch("SetupTooltips")]
   [HarmonyPatch(MethodType.Normal)]
@@ -38,10 +23,36 @@ namespace TravelInfoTooltips{
       try {
         StringBuilder text = new StringBuilder();
         Log.M?.TWL(0, $"travel to {__instance.simState.Starmap.CurSelected.System.Def.Description.Id}");
+        int starSystemIndex = 1;
+        List<string> lines = new List<string>();
+        StringBuilder jumps = new StringBuilder();
+        int jumpsCost = 0;
+        int jumpsCount = 0;
         foreach (var travelEntry in __instance.simState.Starmap.PendingTravelOrder.SubEntries) {
-          Log.M?.WL(1,$"{travelEntry.Description} cost:{travelEntry.Cost}");
-          text.AppendLine(new Localize.Text("{0} {1}", travelEntry.Description, $"{travelEntry.Cost} Days").ToString());
+          Log.M?.WL(1, $"{travelEntry.Description} cost:{travelEntry.Cost}");
+          if (Core.settings.foldJumps == false) {
+            lines.Add(new Localize.Text("{0} {1}", travelEntry.Description, $"{travelEntry.Cost} Days").ToString());
+          } else {
+            if(travelEntry.Type != WorkOrderType.TravelJumping) {
+              lines.Add(new Localize.Text("{0} {1}", travelEntry.Description, $"{travelEntry.Cost} Days").ToString());
+            } else {
+              jumpsCost += travelEntry.Cost;
+              ++jumpsCount;
+              if (starSystemIndex < __instance.simState.Starmap.PotentialPath.Count) {
+                jumps.Append(__instance.simState.Starmap.PotentialPath[starSystemIndex].System.Def.Description.Name);
+                ++starSystemIndex;
+                if(starSystemIndex < (__instance.simState.Starmap.PotentialPath.Count)) {
+                  jumps.Append(", ");
+                }
+              }
+            }
+          }
+          //text.AppendLine(new Localize.Text("{0} {1}", travelEntry.Description, $"{travelEntry.Cost} Days").ToString());
         }
+        if (Core.settings.foldJumps) {
+          lines.Insert(1,new Localize.Text("Jumps: count {0}, {1}, systems: {2}", jumpsCount, $"{jumpsCost} Days", jumps.ToString()).ToString());
+        }
+        foreach (var line in lines) { text.AppendLine(line); }
         BaseDescriptionDef tooltipData = new BaseDescriptionDef("TRAVEL_COST_TOOLTIP_ID","TRAVEL TIME", text.ToString(), string.Empty);
         foreach(var txt in __instance.SystemTravelTime) {
           Image img = txt.gameObject.GetComponentInChildren<Image>();
@@ -70,6 +81,7 @@ namespace TravelInfoTooltips{
   }
   public class Settings {
     public bool debugLog { get; set; }
+    public bool foldJumps { get; set; } = true;
     public Settings() { debugLog = true; }
   }
   public static class Core {
